cc306c2937cda7c4a01ff1525fdfd7541a9465bc
INarynbaev <INarynbaev@beeline.kz>
2021-12-13 18:21:02 +0600

    first commit

---
+++ src/com/company/One/Main.java
@@ -0,0 +1,21 @@
+package com.company.One;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Description:
+        //Write function bmi that calculates body mass index (bmi = weight / height2).
+        //
+        //if bmi <= 18.5 return "Underweight"
+        //
+        //if bmi <= 25.0 return "Normal"
+        //
+        //if bmi <= 30.0 return "Overweight"
+        //
+        //if bmi > 30 return "Obese"
+
+        Calculate.bmi(100,140);
+    }
+}
